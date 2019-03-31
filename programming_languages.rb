1d6516aed2b5b1ae4a697e7af6fb8897b50c4c4c
mario rodrigueza <mariorodriguezan@gmail.com>
2019-03-31 08:27:12 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,13 +1,17 @@
 def reformat_languages(languages)
-
- new_hash = {}
-
- new_hash = :ruby
-
- new_hash[:ruby] = {:type => "interpreted"}
-
- new_hash[:ruby] = {:style => [:00]}  # your code here
-
+ def reformat_languages(languages)
+  # your code here
+  new_hash = {}
+  languages.each do |style,language|
+    language.each do |type,str|
+    if new_hash.has_key?(type)
+      new_hash[type][:style]<< style
+    else
+      new_hash[type] = str
+      new_hash[type][:style] = [style]
+    end
+    end
+  end
+  new_hash
 end
-
 end
